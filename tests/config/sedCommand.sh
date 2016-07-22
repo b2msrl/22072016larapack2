50bccae711df930ffda1a2153c2660c2a103967d
alevento <a@a.it>
2016-07-22 14:25:48 +0200

    Substitute

--- tests/config/sedCommand.sh
+++ tests/config/sedCommand.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i 's/"psr-4": {/"psr-4": { "@@@namespacevendor\\\\@@@namespacepackage_name\\\\Test\\\\": ".\/vendor\/@@@vendor\/@@@package_name\/tests\/",/g' ./composer.json
+sed -i 's/"psr-4": {/"psr-4": { "Padosoft\\\\22072016larapack2\\\\Test\\\\": ".\/vendor\/padosoft\/22072016larapack2\/tests\/",/g' ./composer.json
